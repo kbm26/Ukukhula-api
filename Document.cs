887793cabe43640531e448d9eec4709bb80930be
BoipeloMorokolo <90400639+BoipeloMorokolo@users.noreply.github.com>
2024-02-17 15:08:31 +0200

    Add files via upload
    Added Document Model

---
+++ Document.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Model
+{
+    public class Document
+    {
+        [Key]
+        private int DocumentID;
+        [Required]
+        public String Transcript { get; set; }
+        [Required]
+        public String IdentityDocument { get; set; }
+        [Required]
+        private int ApplicationID { get; set; }
+
+    }
+}
