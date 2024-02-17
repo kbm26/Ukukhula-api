e5e318cdc9da92037b9f2a5abd1ca0f86eb3b262
“Kyle <kyle.moodley@bbd.co.za>
2024-02-17 14:45:07 +0200

    connected to db and create models

---
+++ API/Model/BBDFund.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Model
+{
+    public class BBDFund
+    {
+        [Key]
+        private int FundID;
+        [Required]
+        public int Budget { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [Required]
+        private int UniversityID { get; set; }
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,14 +1,24 @@
+using Microsoft.AspNetCore.Mvc.NewtonsoftJson;
+using Newtonsoft.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+/*
+builder.Services.AddControllersWithViews()
+.AddNewtonsoftJson(options =>
+{
+    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+});
+*/
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
