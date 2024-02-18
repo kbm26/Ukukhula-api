0612948bd56969d4262b094b8329b14c46f59397
BoipeloMorokolo <90400639+BoipeloMorokolo@users.noreply.github.com>
2024-02-18 14:29:38 +0200

    Add files via upload
    Created Repositories as well as Models

---
+++ NewProject/API/Model/StudentInformation.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.Model
+{
+    public class StudentInformation(int IDNumber, DateTime BirthDate, int Age, int Gender, int UserID, int RaceID)
+    {
+        [Key]
+        public int StudentID;
+        [Required]
+        public int IDNumber { get; set; }
+        [Required]
+        public DateTime BirthDate { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public int Gender { get; set; }
+        [Required]
+        public int UserID { get; set; }
+        [Required]
+        public int RaceID { get; set; }
+
+    }
+}
